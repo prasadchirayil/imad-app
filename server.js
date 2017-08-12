9302bdfb2e1b3bf5ecd8ef09fb6c36d857439c91
prasadchirayil <30730843+prasadchirayil@users.noreply.github.com>
2017-08-12 23:12:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,17 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+
 app.get('/article-one', function (req, res) {
+   res.send('article one will be erved');
+});
+
+app.get('/article-two', function (req, res) {
+   res.send('article one will be erved');
+});
+
+app.get('/article-three', function (req, res) {
    res.send('article one will be erved');
 });
 
